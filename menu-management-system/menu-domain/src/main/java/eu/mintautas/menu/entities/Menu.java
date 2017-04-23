da2edf5e81dbf37301166a313ff9e6079a5e7dad
Mintautas Kiulkys <mintautas0@gmail.com>
2017-04-23 20:46:20 +0300

    Updated docs.

--- menu-management-system/menu-domain/src/main/java/eu/mintautas/menu/entities/Menu.java
+++ menu-management-system/menu-domain/src/main/java/eu/mintautas/menu/entities/Menu.java
@@ -9,7 +9,6 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
 /**
  * {@link Menu} class represents a distinct menu a restaurant can have.
  * @author mkiulkys
